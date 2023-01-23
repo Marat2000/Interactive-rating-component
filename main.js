ec92edb3210bca2a9cd6f99925f7e6d89d37b5fa
Marat2000 <122987090+Marat2000@users.noreply.github.com>
2023-01-23 16:36:45 +0400

    Add files via upload
    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+document.getElementById("thanks-state").style.display="none";
+
+
+document.getElementById("one").addEventListener('click',function a(){
+	document.getElementById("select").innerHTML=1});
+
+
+
+document.getElementById("two").addEventListener('click',function a(){
+	document.getElementById("select").innerHTML=2});
+
+
+document.getElementById("three").addEventListener('click',function a(){
+	document.getElementById("select").innerHTML=3});
+
+
+document.getElementById("four").addEventListener('click',function a(){
+	document.getElementById("select").innerHTML=4});
+
+
+document.getElementById("five").addEventListener('click',function a(){
+	document.getElementById("select").innerHTML=5});
+	
+	
+document.getElementById("submit-button").addEventListener('click',function a(){
+document.getElementById("rating-state").style.display="none";
+document.getElementById("thanks-state").style.display="block"});
+	
+
+
+
+
